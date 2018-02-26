ca3cec8019162d8a05c43b4425f41e312d7d2d60
Rudi Lee <quadhack@gmail.com>
2018-02-26 17:49:47 +0700

    Adding setup & teardown for the asterisk object lifespan management Adding test for failed login

--- test/AMILoginActionTests.ts
+++ test/AMILoginActionTests.ts
@@ -1,14 +1,32 @@
 import {AsteriskManagerInterface as AMI} from "..";
 
-test("send Login action with correct credentials get Success response from AMI", done => {
-    let asterisk = new AMI;
+beforeEach(() => {
+    this.asterisk = new AMI;
+});
 
-    asterisk.connect(5038, "192.168.99.128", () => {
-        asterisk.login({
+afterEach(() => {
+    this.asterisk.end();
+});
+
+test("send Login action with correct credentials get Success response from AMI", done => {
+    this.asterisk.connect(5038, "192.168.99.128", () => {
+        this.asterisk.login({
             Username: "test",
             Secret: "12345"
         }, (response, headers) => {
             expect(response).toBe("Success");
+            done();
+        });
+    });
+});
+
+test("send Login action with incorrect credentials get Error response from AMI", done => {
+    this.asterisk.connect(5038, "192.168.99.128", () => {
+        this.asterisk.login({
+            Username: "tests",
+            Secret: "123456"
+        }, (response, headers) => {
+            expect(response).toBe("Error");
             done();
         });
     });
